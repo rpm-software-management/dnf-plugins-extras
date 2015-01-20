52752ffb6c326a0a56e8652c94dc6013751b8eb2
Jakub Kadlčík <frostyx@email.cz>
2015-01-20 20:42:31 +0100

    Change indentation from tabs to four spaces

--- plugins/tracer.py
+++ plugins/tracer.py
@@ -28,57 +28,57 @@ from tracer.resources.lang import _
 
 
 class Tracer(dnf.Plugin):
-	"""DNF plugin for `tracer` command"""
-	name = 'tracer'
+    """DNF plugin for `tracer` command"""
+    name = 'tracer'
 
-	def __init__(self, base, cli):
-		self.timestamp = time.time()
-		self.base = base
-		self.cli = cli
-		if self.cli is not None:
-			self.cli.register_command(TracerCommand)
+    def __init__(self, base, cli):
+        self.timestamp = time.time()
+        self.base = base
+        self.cli = cli
+        if self.cli is not None:
+            self.cli.register_command(TracerCommand)
 
-	def transaction(self):
-		"""
-		Call after successful transaction
-		See https://rpm-software-management.github.io/dnf/api_transaction.html
-		"""
-		installed = set([package.name for package in self.base.transaction.install_set])
-		erased = set([package.name for package in self.base.transaction.remove_set])
+    def transaction(self):
+        """
+        Call after successful transaction
+        See https://rpm-software-management.github.io/dnf/api_transaction.html
+        """
+        installed = set([package.name for package in self.base.transaction.install_set])
+        erased = set([package.name for package in self.base.transaction.remove_set])
 
-		# Don't run tracer when uninstalling it
-		if "tracer" in erased - installed:
-			return
+        # Don't run tracer when uninstalling it
+        if "tracer" in erased - installed:
+            return
 
-		args = ['tracer', '-n'] + list(installed | erased)
-		p = subprocess.Popen(args, stdout=subprocess.PIPE)
-		out = p.communicate()[0]
-		_print_output(out)
+        args = ['tracer', '-n'] + list(installed | erased)
+        p = subprocess.Popen(args, stdout=subprocess.PIPE)
+        out = p.communicate()[0]
+        _print_output(out)
 
-		if len(out) != 0:
-			print "\n" + _("for_more_informations")
-			print "    sudo tracer -iat " + str(self.timestamp)
+        if len(out) != 0:
+            print "\n" + _("for_more_informations")
+            print "    sudo tracer -iat " + str(self.timestamp)
 
 
 class TracerCommand(dnf.cli.Command):
-	"""DNF tracer plugin"""
-	aliases = ['tracer']
-	activate_sack = True
+    """DNF tracer plugin"""
+    aliases = ['tracer']
+    activate_sack = True
 
-	def run(self, args):
-		"""Called after running `dnf tracer ...`"""
-		args = ['tracer'] + args
-		p = subprocess.Popen(args, stdout=subprocess.PIPE)
-		out = p.communicate()[0]
-		_print_output(out)
+    def run(self, args):
+        """Called after running `dnf tracer ...`"""
+        args = ['tracer'] + args
+        p = subprocess.Popen(args, stdout=subprocess.PIPE)
+        out = p.communicate()[0]
+        _print_output(out)
 
 
 def _print_output(out):
-	if len(out) == 0:
-		print _("you_should_restart")
-		print "  " + _("nothing_to_restart")
-		return
+    if len(out) == 0:
+        print _("you_should_restart")
+        print "  " + _("nothing_to_restart")
+        return
 
-	# Last value is blank line
-	for line in out.split('\n')[:-1]:
-		print line
+    # Last value is blank line
+    for line in out.split('\n')[:-1]:
+        print line
