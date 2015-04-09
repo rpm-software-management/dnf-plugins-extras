e764073c9101b7402c3b5325a164b3cb12ce1ce4
Michael Mraka <michael.mraka@redhat.com>
2015-04-09 19:55:32 +0200

    trans_end record may not exist (RhBug: 1209043)

--- plugins/migrate.py
+++ plugins/migrate.py
@@ -178,7 +178,8 @@ class MigrateCommand(dnf.cli.Command):
             if t.cmdline:
                 dnf_cur.execute("""insert into trans_cmdline
                     (tid, cmdline) values (?, ?)""", (dnf_tid, t.cmdline))
-            dnf_cur.execute("""insert into trans_end
+            if t.end_timestamp:
+                dnf_cur.execute("""insert into trans_end
                     (tid, timestamp, rpmdb_version, return_code)
                     values (?, ?, ?, ?)""",
                             (dnf_tid, t.end_timestamp, t.end_rpmdbversion,
